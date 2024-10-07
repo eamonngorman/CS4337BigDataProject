d5b63c6dcdebff5a68e170412c2da7903f0031ff
Eamonn <eamonngorman21@gmail.com>
2024-10-07 18:47:04 +0100

    Initial Commit of SpringBoot Application created with Spring Initializr with Spring Web API dependency and Java 23

---
+++ src/test/java/com/example/BigDataProject/BigDataProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BigDataProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
